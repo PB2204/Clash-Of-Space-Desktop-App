de21029c4f628ac5fa7bf86c272abad27a3f226c
Pabitra Banerjee <rockstarpabitra2204@gmail.com>
2024-03-31 03:22:06 +0530

    initial commit

---
+++ cos-ms-win32-x64/resources/app/main.js
@@ -0,0 +1,27 @@
+const { app, BrowserWindow } = require('electron/main')
+const path = require('path')
+
+const createWindow = () => {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600
+  })
+
+  win.loadFile(path.join('public/index.html'));
+}
+
+app.whenReady().then(() => {
+  createWindow()
+
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+      createWindow()
+    }
+  })
+})
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+})
